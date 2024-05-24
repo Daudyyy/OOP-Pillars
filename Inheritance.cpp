5b3a20fcea983267bea0a96ad6a9198c492d86a4
Daudyyy <chaudharydawood68@gmail.com>
2024-05-24 17:19:31 +0500

    Multi-Level

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -27,3 +27,61 @@ male object1;
 cout << object1.height << endl;
 return 0
 };
+
+//Single Inheritance
+
+class Animal{
+
+    public:
+    int age;
+    int weight;
+
+    public:
+    void speak(){
+        cout << "speaking" << endl;
+    };
+
+class Dog: public Animal{
+
+};
+
+int main(){
+    Dog d;
+    d.speak();
+    cout << d.age << endl;
+
+    return 0;
+}
+
+}
+
+//Multi-Level
+
+class Animal{   //first class
+
+    public:
+    int age;
+    int weight;
+
+    public:
+    void speak(){
+        cout << "speaking" << endl;
+    };
+
+class Dog: public Animal{   //second class
+
+};
+
+class Puppy: public Dog{   //third class
+
+};
+
+int main(){
+    Puppy p;
+    p.speak();
+    cout << d.age << endl;
+
+    return 0;
+}
+
+}
